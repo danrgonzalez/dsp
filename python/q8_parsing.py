a3f22bddebf4427b43b8c5253a7bbce064ef69f1
danrgonzalez <danrgonzalez@gmail.com>
2016-03-06 14:24:31 -0800

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -6,14 +6,48 @@
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
-
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+parsed_data = []
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+def read_data(data):
+   with open(data, 'rb') as csvfile:
+       reader = csv.reader(csvfile)
+       for row in reader:
+          print row
+          parsed_data.append(row)
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_min_score_difference(parsed_data):
+    #print parsed_data[0][5], parsed_data[0][6]
+    
+    diff = []
+    for i in range(1,len(parsed_data)):
+        #print int(parsed_data[i][5]), int(parsed_data[i][6])
+        d = int(parsed_data[i][5]) - int(parsed_data[i][6])
+        #print d
+        diff.append(d)        
+        #print diff
+    
+    print min(diff)
+    
+
+def get_team(parsed_data):
+    diff = []
+    
+    for i in range(1,len(parsed_data)):
+        #print int(parsed_data[i][5]), int(parsed_data[i][6])
+        d = int(parsed_data[i][5]) - int(parsed_data[i][6])
+        #print d
+        diff.append(d)        
+        #print diff
+    
+    print diff.index(min(diff))+1
+    print parsed_data[diff.index(min(diff))+1][0]
+    
+
+
+read_data('football.csv')
+
+get_min_score_difference(parsed_data)
+
+get_team(parsed_data)
